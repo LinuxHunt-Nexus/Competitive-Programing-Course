c40691916465271f7566f8136ab5fefd605b32ec
LinuxHunt-Nexus <linuxhuntnexus@gmail.com>
2024-09-28 10:34:03 +0600

    Function and return type

--- 1.cpp
+++ 1.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
+int sum(int n)
+{
+    int digit_sum=0;
+    while (n)
+    {
+        digit_sum = digit_sum + n%10;
+        n=n/10;
+        /* code */
+    }
+    return digit_sum;
+}
 
 int main() {
+     int a, b;
+     cin>>a >> b;
+     cout << sum(a) + sum(b);
     
 }
