f41ef4489602e7282e9afd95035b9455f160f726
LinuxHunt-Nexus <linuxhuntnexus@gmail.com>
2024-09-28 10:33:31 +0600

    starting from reference

--- 1.cpp
+++ 1.cpp
@@ -2,21 +2,7 @@
 
 using namespace std;
 
-int sum(int n)
-{
-    int digit_sum=0;
-    while (n)
-    {
-        digit_sum = digit_sum + n%10;
-        n=n/10;
-        /* code */
-    }
-    return digit_sum;
-}
 
 int main() {
-     int a, b;
-     cin>>a >> b;
-     cout << sum(a) + sum(b);
     
 }
